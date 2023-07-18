43d8e3db88b929959bd75296bdbc4fd4968dc876
Nikita Volodin <volodin.n@gmail.com>
2023-07-17 21:55:17 -0400

    fix: handle error accessing github api

--- src/utils/export-last-update-at-env.ts
+++ src/utils/export-last-update-at-env.ts
@@ -11,24 +11,29 @@ export default async function exportLastUpdateAtEnv(): Promise<{
   const context = github.context;
   const secrets = getSecrets();
   const token = secrets.GITHUB_TOKEN;
-
   const octokit = github.getOctokit(token);
-  const result = await octokit.rest.actions.listWorkflowRuns({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    workflow_id: ACTIONSFLOW_WORKFLOW_FILE_NAME as unknown as number,
-    per_page: 2,
-  });
+
   let createAtTime: number | undefined;
-  if (result && result.data && result.data.workflow_runs) {
-    const workflow_runs = result.data.workflow_runs;
-    if (workflow_runs[1] && workflow_runs[1].created_at) {
-      const createAt = workflow_runs[1].created_at;
-      createAtTime = new Date(createAt).getTime();
-      core.debug(`export env ${ACTIONSFLOW_LAST_UPDATE_AT}: ${createAtTime}`);
-      core.exportVariable(ACTIONSFLOW_LAST_UPDATE_AT, createAtTime);
+  try {
+    const result = await octokit.rest.actions.listWorkflowRuns({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      workflow_id: ACTIONSFLOW_WORKFLOW_FILE_NAME as unknown as number,
+      per_page: 2,
+    });
+    if (result && result.data && result.data.workflow_runs) {
+      const workflow_runs = result.data.workflow_runs;
+      if (workflow_runs[1] && workflow_runs[1].created_at) {
+        const createAt = workflow_runs[1].created_at;
+        createAtTime = new Date(createAt).getTime();
+        core.debug(`export env ${ACTIONSFLOW_LAST_UPDATE_AT}: ${createAtTime}`);
+        core.exportVariable(ACTIONSFLOW_LAST_UPDATE_AT, createAtTime);
+      }
     }
+  } catch (e) {
+    core.debug("Could not determine last time this workflow was executed");
   }
+
   return {
     [ACTIONSFLOW_LAST_UPDATE_AT]: createAtTime,
   };
